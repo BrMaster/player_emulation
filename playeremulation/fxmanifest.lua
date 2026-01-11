b16ee59f761c8b598e769dc58964394092fb2f52
BranikDEV <75860536+BrMaster@users.noreply.github.com>
2026-01-11 16:48:56 +0100

    Add files via upload

---
+++ playeremulation/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+-- Resource Information
+name 'Player Emulation'
+description 'A comprehensive player emulation script with full control over NPC behavior'
+author 'branikdev'
+contributors 'none'
+version '1.5'
+
+-- Scripts
+client_script 'client.lua'
+
+-- Dependencies (none required)
+-- dependencies {}
+
+-- ===========================================
